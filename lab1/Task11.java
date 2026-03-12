3b70223f4b138e86d580664fdddcf908290a5a52
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2026-03-12 19:52:11 +0200

    add lab1

---
+++ lab1/Task11.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        System.out.println("Сорочан Ярослав Сергійович, 11 варіант");
+
+        int[][] A = new int[3][3];
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                A[i][j] = random.nextInt(100);
+            }
+        }
+
+        System.out.println("- матриця А (до обробки);");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введіть i: ");
+        int i = scanner.nextInt();
+        System.out.print("Введіть j: ");
+        int j = scanner.nextInt();
+
+        System.out.println("- матриця А (після обробки);");
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(A[row][col] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("- значення a_ij матриці А: " + A[i][j]);
+
+        scanner.close();
+    }
+}
